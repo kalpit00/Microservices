5838cbcee031f8f0abbee71dde750e8af9aa504f
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-10-13 09:57:30 +0530

    Add Product service

---
+++ product-service/src/test/java/com/kalpit00/product/ProductServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kalpit00.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
