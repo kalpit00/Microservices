f1752aa55322e4087dee30af6a72e1227be06dcf
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-10-15 14:44:41 +0530

    CircuitBreaker Added to API GateWay

--- order-service/src/main/java/com/kalpit00/order/client/InventoryClient.java
+++ order-service/src/main/java/com/kalpit00/order/client/InventoryClient.java
@@ -1,10 +1,14 @@
 package com.kalpit00.order.client;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 
 public interface InventoryClient {
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam(name = "skuCode") String skuCode,
                       @RequestParam(name = "quantity") Integer quantity);
 }
