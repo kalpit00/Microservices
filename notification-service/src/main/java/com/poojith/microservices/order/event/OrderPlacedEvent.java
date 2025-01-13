f89535d16d800f49d4c4282155ccb4968a1ddafd
Kalpit Patel <kalpit.k.patel@gmail.com>
2025-01-13 03:21:11 -0500

    Notification Service added with Kafka!
    Added Kafka Templates to produce and consume events. Notification Service sends emails to user using Java EMailSender

---
+++ notification-service/src/main/java/com/poojith/microservices/order/event/OrderPlacedEvent.java
@@ -0,0 +1,30 @@
+package com.poojith.microservices.order.event;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderPlacedEvent {
+    private String orderNumber;
+    private String email;
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ notification-service/src/main/java/com/poojith/microservices/service/NotificationService.java
@@ -0,0 +1,47 @@
+package com.poojith.microservices.service;
+
+import com.poojith.microservices.order.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent){
+        log.info("Got Message from order-placed topic {}", orderPlacedEvent);
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springshop@email.com");
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
+            messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
+            messageHelper.setText(String.format("""
+                            Hi 
+
+                            Your order with order number  is now placed successfully.
+                            
+                            Best Regards
+                            Spring Shop
+                            """,
+
+                    orderPlacedEvent.getOrderNumber()));
+        };
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order Notifcation email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new RuntimeException("Exception occurred when sending mail to springshop@email.com", e);
+        }
+    }
+}
